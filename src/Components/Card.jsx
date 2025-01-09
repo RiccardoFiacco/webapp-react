1f876a6c76ae4171b669eae10c831ce1021944a5
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-09 17:36:33 +0100

    add flex wrap

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,8 +8,8 @@ export function Card({props}){
         <div className="relative ">
             <img src={img ? img : ''}/>
             
-            <div className="absolute bottom-0 text-white bg-cyan-300/80 flex justify-between w-full py-4 px-4">
-             <div className="text-neutral-950 text-3xl">{title}</div>
+            <div className="absolute bottom-0 text-white bg-cyan-300/80 flex flex-wrap justify-between w-full py-4 px-4">
+             <div className="text-neutral-950 text-3xl sm:text-md">{title}</div>
              <Link to={`/${id}`} className=" bg-slate-600 text-white rounded-md px-2 hover:bg-slate-800">dettagli</Link>
             </div>
             
