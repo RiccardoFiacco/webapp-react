ebacfcfcc9d26ad7db9e246d720ddc322652e420
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-09 23:58:00 +0100

    implement search bar

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,18 +1,26 @@
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Card } from './Card'
+import { GlobalContext } from "../GlobalContext"
 export function Movies(){
+    const { search } = useContext(GlobalContext)
     let baseUrl = 'http://localhost:3000/api/movies'
     const [movies, setMovies] = useState([])
-
+    
     useEffect(()=>{
-        axios
-        .get(baseUrl)
-        .then((res)=>{
-            setMovies(res.data)
-        })
-        .catch((err)=>{console.log(err)})
-    },[baseUrl])
+        setTimeout(()=>{
+            axios
+            .get(baseUrl,{
+                params: {
+                    title: search,
+                }
+            })
+            .then((res)=>{
+                setMovies(res.data)
+            })
+            .catch((err)=>{console.log(err)})
+        }, 1000)
+    },[baseUrl, search])
 
     return(
         <div className="grid grid-cols-4 gap-y-10 gap-x-10 items-center">
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,37 @@
 import { Movies } from '../Components/Movies'
-
+import { useEffect, useState} from 'react'
+import { GlobalContext } from '../GlobalContext.js';
 export function Home(){
-    return(
-    <div className="container grow flex flex-col py-8"> 
-  
-    <div className="flex justify-between items-center py-3 ">
-        <div>
-         <h1 className="text-3xl font-bold">Benvenuti su movies review </h1>
-         <p>Uno spazio dove poter recensire vari film in totale liberta</p>
-        </div>
-        
-        <div>
-         <input className="h-8 bg-slate-500 rounded-md border-2 border-slate-950" type="text" placeholder='Cerca'/>
-        </div>
-    </div>
+    const [search, setSearch] = useState('')
 
-    <Movies/> 
-    </div>
+    function changeHandler(e){
+        setSearch(e.target.value)
+    }
+
+    useEffect(()=>{
+
+    },[search])
+
+    return(
+    <GlobalContext.Provider value={{search}}>
+     <div className="container grow flex flex-col py-8"> 
+      <div className="flex justify-between items-center py-3 ">
+          <div>
+           <h1 className="text-3xl font-bold">Benvenuti su movies review </h1>
+           <p>Uno spazio dove poter recensire vari film in totale liberta</p>
+          </div>
+          
+          <div>
+           <input 
+           onChange={(e)=>changeHandler(e)} 
+           className="h-8 bg-slate-500 rounded-md border-2 border-slate-950" 
+           type="text" 
+           value={search}
+           placeholder='Cerca'/>
+          </div>
+      </div>
+     <Movies/> 
+     </div>
+    </GlobalContext.Provider>
     )
 }
