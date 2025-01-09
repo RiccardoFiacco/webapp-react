059459813c63d7e9201998c79f890af041d7dd21
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-10 00:03:10 +0100

    change details button

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,9 +8,9 @@ export function Card({props}){
         <div className="relative">
             <img src={img ? img : ''}/>
             
-            <div className="absolute bottom-0 text-white bg-cyan-300/80 flex flex-wrap justify-between w-full py-4 px-4">
+            <div className="absolute bottom-0 text-white bg-cyan-300/80 flex flex-wrap justify-between items-center w-full py-4 px-4">
              <div className="text-neutral-950 text-3xl sm:text-md">{title}</div>
-             <Link to={`/${id}`} className=" bg-slate-600 text-white rounded-md px-2 hover:bg-slate-800">dettagli</Link>
+             <Link to={`/${id}`} className=" bg-slate-600 text-white rounded-md px-2 hover:bg-slate-800 h-7">dettagli</Link>
             </div>
             
         </div>   
