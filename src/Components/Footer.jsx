f5b06e56b788aa8ef57ffb92f327a598e6a37db4
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-09 15:20:41 +0100

    done the change

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,14 @@
 export function Footer(){
     return(
-       <div>
-           Footer
-       </div>
+        <div className="bg-cyan-300 py-6">
+         <div className="container flex justify-between items-center">
+            <span>movies review</span>
+            <div className="flex items-center gap-6">
+                <p>latina</p>
+                <p>09/01/25</p>
+                <p>done by riccardo</p>
+            </div>
+         </div>   
+        </div>
     )
    }
