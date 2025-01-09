213733fb13e73002638fe2de4e5d3d510d0005a3
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-09 13:20:12 +0100

    add base default layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
  
-
   return (
     <>
+    <BrowserRouter>
+     <Routes Component={DefaultLayout}>
+      <Route/>
+      <Route/>
+      <Route/>
+     </Routes>
+    </BrowserRouter>
+    
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1><p>ciao</p>
---
+++ src/Layout/DefaultLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+export function DefaultLayout(){
+    return(
+        <>
+         <Headers/>
+         <Outlet/>
+         <Footer/>
+        </>
+    )
+}
