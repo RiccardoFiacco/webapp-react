935abb50ee0b80c3ce9cd5779f72093b074c24ed
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-09 14:57:29 +0100

    add home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export function Home(){
+    return(
+    <div className="grow">
+     <h1 className="text-3xl font-bold underline"> Benvenuti su movies review </h1>
+     <p>uno spazio dove poter recensire vari film in totale liberta</p>
+    </div>
+    )
+}
