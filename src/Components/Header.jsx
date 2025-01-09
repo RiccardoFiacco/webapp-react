c212457dd37a083c3473d78baa71d1e1028fdbe5
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-09 15:20:58 +0100

    done the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 export function Header(){
  return(
-    <div>
-       Header 
+    <div className="bg-cyan-300 py-6">
+        <div className="container ">
+            <Link to='/' className="text-3xl bg-slate-600 text-white rounded-md px-2 hover:bg-slate-800">MOVIES REVIEW</Link>
+        </div>   
     </div>
  )
 }
