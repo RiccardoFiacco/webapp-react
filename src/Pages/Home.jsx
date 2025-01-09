b79f2b3e1f4c257f07f24cf5e25aea8a436b6149
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-09 15:53:49 +0100

    add input button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,14 +2,20 @@ import { Movies } from '../Components/Movies'
 
 export function Home(){
     return(
-    <div className="grow flex flex-col items-center"> 
+    <div className="container grow flex flex-col"> 
   
-    <div className="flex flex-col items-center py-8">
-     <h1 className="text-3xl font-bold underline">Benvenuti su movies review </h1>
-     <p>uno spazio dove poter recensire vari film in totale liberta</p>
+    <div className="flex justify-between items-center py-8">
+        <div>
+         <h1 className="text-3xl font-bold">Benvenuti su movies review </h1>
+         <p>Uno spazio dove poter recensire vari film in totale liberta</p>
+        </div>
+
+        <div>
+         <input className="h-8 bg-slate-500 rounded-md border-2 border-slate-950" type="text" placeholder='Cerca'/>
+        </div>
     </div>
-     
-     <Movies/>
+
+    <Movies/> 
     </div>
     )
 }
