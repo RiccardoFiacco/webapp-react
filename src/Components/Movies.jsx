88c3434951912943930851fa3a99ec5dbb494523
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-09 16:02:46 +0100

    add axios call to take all movies

---
+++ src/Components/Movies.jsx
@@ -0,0 +1,21 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+export function Movies(){
+    let baseUrl = 'http://localhost:3000/api/movies'
+    const [movies, setMovies] = useState([])
+
+    useEffect(()=>{
+        axios
+        .get(baseUrl)
+        .then((res)=>{
+            setMovies(res.data)
+        })
+        .catch((err)=>{console.log(err)})
+    },[baseUrl])
+
+    return(
+        <div className="grid grid-cols-4 gap-y-10 gap-x-10 items-center">
+        </div>
+    )
+}
