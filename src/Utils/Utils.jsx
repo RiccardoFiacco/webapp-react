90d0c1d24dcb7beba031bcd67b6f392993134548
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-10 18:19:28 +0100

    add axiosCall function

--- src/Utils/Utils.jsx
+++ src/Utils/Utils.jsx
@@ -1,6 +1,26 @@
 import { faStar } from "@fortawesome/free-solid-svg-icons"
 import { faStar as starRegular } from "@fortawesome/free-regular-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import axios from "axios";
+
+export const baseUrl = 'http://localhost:3000/api/movies';
+export const sendUrl = 'http://localhost:3000/api/reviews';
+export const imgUrl = 'http://localhost:3000/photo/';
+
+export function axiosCall(url, callback, param){
+    
+        axios
+        .get(baseUrl, param ? {
+            params: {
+                title: param,
+            }
+        }:'')
+        .then((res)=>{
+            callback(res.data)
+        })
+        .catch((err)=>{console.log(err)})
+    
+}
 
 export function getStar(vote){
     let app = Math.ceil(vote/2);
