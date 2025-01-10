a0fbe1b8a831e757f71e0e2ed02d606199fc9cfd
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-10 18:18:35 +0100

    add global context in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,24 @@ import { DefaultLayout } from './Layout/DefaultLayout'
 import { Home } from './Pages/Home'
 import { Details } from './Pages/Details.jsx'
 import { Routes } from 'react-router-dom'
+import { GlobalContext } from './Utils/GlobalContext.js'
+import { useState, useEffect } from 'react'
+import { axiosCall } from './Utils/Utils.jsx'
+import { baseUrl } from './Utils/Utils.jsx'
+
 function App() {
+ const [search, setSearch] = useState('')
+ const [movies, setMovies] = useState([])
  
+  useEffect(()=>{
+      axiosCall(baseUrl, setMovies)
+  },[])
+
   return (
-    <>
+    <GlobalContext.Provider value={{
+      search, setSearch,
+      movies, setMovies,
+    }}>
      <BrowserRouter>
       <Routes>
        <Route Component={DefaultLayout}>
@@ -16,7 +30,7 @@ function App() {
        </Route>
       </Routes>
      </BrowserRouter>
-    </>
+    </GlobalContext.Provider>
   )
 }
 
