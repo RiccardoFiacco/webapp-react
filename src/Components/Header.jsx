7aa568db6e8dc4e789dbaa63eb697fde36e0f288
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-10 12:09:27 +0100

    change header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,12 @@ import { Link } from "react-router-dom"
 export function Header(){
  return(
     <div className="bg-cyan-300 py-6">
-        <div className="container ">
+        <div className="container flex justify-between">
             <Link to='/' className="text-3xl bg-slate-600 text-white rounded-md px-2 hover:bg-slate-800">MOVIES REVIEW</Link>
+            <div className="flex items-center gap-6">
+                <Link to='/' className="hover:text-amber-500">home</Link>
+                <Link to='/' className="hover:text-amber-500">about</Link>
+            </div>
         </div>   
     </div>
  )
