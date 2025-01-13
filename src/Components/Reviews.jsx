8bf7f397d24d43dd2b385d1abd475bc8037e72bd
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-13 13:09:07 +0100

    add delete button

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,5 +1,5 @@
 export function Reviews({reviews}){
-
+    
     return(
         <>
     {reviews && reviews.map(({name, vote, text},i)=>{
@@ -7,7 +7,11 @@ export function Reviews({reviews}){
             <div key={i} className="rounded-lg border-gray-600 border-2 py-3 my-4 px-3">
                 <p>{text}</p>
                 <p>voto: {vote}</p>
-                <p className=" text-gray-500 text-lg font-bold italic ">by {name}</p>
+                <div className="flex justify-between items-center">
+                    <span className=" text-gray-500 text-lg font-bold italic ">by {name}</span>
+                    <button className="bg-cyan-300 rounded-md px-2 h-10 hover:bg-cyan-600">elimina</button>
+                </div>
+                
            </div>
         )
     })}</>
