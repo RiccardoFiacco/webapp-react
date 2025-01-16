41c6aac6e6347e7279ca14f45d34880bb0937588
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-16 21:18:04 +0100

    add comment

--- src/Components/WithFormLogin.jsx
+++ src/Components/WithFormLogin.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/display-name */
 import axios from "axios";
@@ -6,23 +7,24 @@ import { loginUrl } from '../Utils/Utils'
 import { useContext } from "react";
 import { GlobalContext } from "../Utils/GlobalContext";
 export function WithFormLogin(Component){
-    return ({data, resetForm,  ...other}) => {
-        const {setLogged} = useContext(GlobalContext)
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        let navigate = useNavigate();
+    return ({data, resetForm,  ...other}) => { //destructuring delle props che mi servono, con other prendo le altre e le metto in quella prop
+
+        const {setLogged} = useContext(GlobalContext) //importo setLogged per impostare la variabile 
+        let navigate = useNavigate(); //uso l'hook per creare la variabile per far tornare alla home 
         
         async function sendData(event){
             event.preventDefault();
+            //trim degli input
             data.email = data.email.trim()
             data.password = data.password.trim()
             try{
-                const result = await axios.post(loginUrl+'/login', data)
-                const {loggato, code} = result.data
+                const result = await axios.post(loginUrl+'/login', data) //chiamata alla rotta a cui passo i dati
+                const {loggato, code} = result.data //destructuring della risposta
                 if(loggato){
                  console.log('utente trovato')
-                 setLogged(code)
-                 resetForm();
-                 navigate("/") 
+                 setLogged(code) //setto la variabile reattiva con il valore ricevuto dal be
+                 resetForm(); //resetto i campi a vuoto
+                 navigate("/") //torno alla home
                 }else{
                     console.log('utente non trovato')
                 }
