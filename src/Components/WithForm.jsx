d99c839645fabaa6384520dd73220b162e00b86a
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-16 18:18:37 +0100

    add new prop

--- src/Components/WithForm.jsx
+++ src/Components/WithForm.jsx
@@ -5,7 +5,6 @@ export function WithForm(Component, baseForm){
     return (props)=>{
         // eslint-disable-next-line react-hooks/rules-of-hooks
         const [data, setData] = useState(baseForm)
-
         function changeHandler(event) {
             setData((review) => {
             return {
@@ -14,10 +13,11 @@ export function WithForm(Component, baseForm){
             };
           });
         }
-        console.log(data)
+        
         return <Component //con questo io ritorno il componente arricchito di nuove props, create in questo componente ma usufruibili nell'import  
                 data={data} //passo i valori che dovranno essere mostrati come value
                 handlerInput={changeHandler} //passo il gestore per aggiornare gli input
+                setter = {setData}
                 {...props}/> //queste sono le props che aveva gia il componente  
     }
 }
