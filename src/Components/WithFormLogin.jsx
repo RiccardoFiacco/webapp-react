214b7df15149cad92928adcb15927867b5b412e1
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-16 18:19:11 +0100

    add logic to check the user log in

--- src/Components/WithFormLogin.jsx
+++ src/Components/WithFormLogin.jsx
@@ -2,24 +2,34 @@
 import axios from "axios";
 import { useNavigate } from "react-router";
 import { loginUrl } from '../Utils/Utils'
+import { useContext } from "react";
+import { GlobalContext } from "../Utils/GlobalContext";
 export function WithFormLogin(Component, baseForm){
-    
+
+    const {setLogged} =useContext(GlobalContext)
+
     return (props)=>{
 
         // eslint-disable-next-line react-hooks/rules-of-hooks
         let navigate = useNavigate();
-        const {data, setData} = props 
+        const {data, setter} = props 
         
         async function sendData(event){
             event.preventDefault();
-            console.log(data)
             data.email = data.email.trim()
             data.password = data.password.trim()
             try{
-                const result = await axios.get(loginUrl+'/login', data)
-                console.log(result.data)
-                setData(baseForm)
-                navigate("/")
+                const result = await axios.post(loginUrl+'/login', data)
+                const {loggato, code} = result.data
+                if(loggato){
+                 console.log('utente trovato')
+                 setLogged(code)
+                 setter(baseForm)
+                 navigate("/") 
+                }else{
+                    console.log('utente non trovato')
+                }
+                
             }catch(err){
                 console.log(err)
             }
