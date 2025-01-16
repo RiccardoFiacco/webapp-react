894910cbdcc82150f1ac2b19cb6032b64eec34bf
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-16 18:19:30 +0100

    removed console.log and add right setter

--- src/Components/WithFormRegistration.jsx
+++ src/Components/WithFormRegistration.jsx
@@ -1,26 +1,23 @@
 /* eslint-disable react/display-name */
 import axios from "axios";
-import { useState } from "react"
 import { useNavigate } from "react-router";
 import { loginUrl } from '../Utils/Utils'
 export function WithFormRegistration (Component, baseForm){
     
     return (props)=>{
-        console.log(props)
         // eslint-disable-next-line react-hooks/rules-of-hooks
         let navigate = useNavigate();
-        const {data, setData} = props 
-
+        const {data, setter} = props 
+        
         async function sendData(event){
             event.preventDefault();
-            console.log(props)
-            console.log(data)
             data.email = data.email.trim()
             data.password = data.password.trim()
+            
             try{
                 const result = await axios.post(loginUrl+'/registration', data)
                 console.log(result.data)
-                setData(baseForm)
+                setter(baseForm)
                 navigate("/")
             }catch(err){
                 console.log(err)
