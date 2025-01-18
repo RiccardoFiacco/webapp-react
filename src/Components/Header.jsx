19a2f647c7c2be7b3f9267933ae93a351aee62c6
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-18 18:19:37 +0100

    change button login display logic

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,7 +11,7 @@ export function Header(){
             <Link to='/' className="text-3xl bg-slate-600 text-white rounded-md px-2 hover:bg-slate-800">MOVIES REVIEW</Link>
             <div className="flex items-center gap-6">
                 <Link to='/' className="hover:text-amber-500">home</Link>
-                {logged === '' ? <Link to='/user' className="hover:text-amber-500">login</Link> : <ExitButton/>}
+                {logged ? <ExitButton/>:<Link to='/user' className="hover:text-amber-500">login</Link>  }
             </div>
         </div>   
     </div>
