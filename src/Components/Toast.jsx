84e0bd62293205911d72cbb2ba6a05d01431d319
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-18 23:41:37 +0100

    done more style to the toast

--- src/Components/Toast.jsx
+++ src/Components/Toast.jsx
@@ -4,9 +4,9 @@ import { GlobalContext } from "../Utils/GlobalContext"
 export function Toast(){
     const {toastMsg, setSeeToast, seeToast} =useContext(GlobalContext)
     return(<>
-        {seeToast ? <div className="fixed right-10 bottom-10 bg-slate-200 py-6 px-3 rounded border border-slate-950">
+        {seeToast ? <div className="flex fixed right-10 bottom-10 bg-slate-200 py-3 px-3 rounded border border-l-4 border-l-indigo-500 border-slate-950">
             <div>{toastMsg}</div>
-            <div className="absolute top-1 right-3 border border-slate-950 px-2" onClick={()=>setSeeToast(false)}>X</div>
+            <div className="border border-slate-950  mx-4 px-2 rounded-lg" onClick={()=>setSeeToast(false)}>X</div>
         </div>: null}
         </>
     )
