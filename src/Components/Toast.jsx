a6dbe0c18502fdac0ccd0c6811ee5f1353e13921
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-19 14:24:18 +0100

    add changes on button toast

--- src/Components/Toast.jsx
+++ src/Components/Toast.jsx
@@ -6,7 +6,7 @@ export function Toast(){
     return(<>
         {seeToast ? <div className="flex fixed right-10 bottom-10 bg-slate-200 py-3 px-3 rounded border border-l-4 border-l-indigo-500 border-slate-950">
             <div>{toastMsg}</div>
-            <div className="border border-slate-950  mx-4 px-2 rounded-lg" onClick={()=>setSeeToast(false)}>X</div>
+            <button className="border border-slate-950  mx-4 px-2 rounded-lg" onClick={()=>setSeeToast(false)}>X</button>
         </div>: null}
         </>
     )
