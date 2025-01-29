0f918f3c3f832e62de3d372bff787a018c9d3dab
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-29 17:40:22 +0100

    removed home button

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,7 +10,6 @@ export function Header(){
         <div className="container flex justify-between">
             <Link to='/' className="text-3xl bg-slate-600 text-white rounded-md px-2 hover:bg-slate-800">MOVIES REVIEW</Link>
             <div className="flex items-center gap-6">
-                <Link to='/' className="hover:text-amber-500">home</Link>
                 {logged ? <ExitButton/>:<Link to='/user' className="hover:text-amber-500">login</Link>  }
             </div>
         </div>   
