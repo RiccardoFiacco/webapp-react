ae75e8c712f34dfa60c25fd0c211d860790adcb4
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-29 17:40:56 +0100

    add timeout to toast

--- src/Components/Toast.jsx
+++ src/Components/Toast.jsx
@@ -1,9 +1,15 @@
-import { useContext } from "react"
+/* eslint-disable react/prop-types */
+import { useContext, useEffect} from "react"
 import { GlobalContext } from "../Utils/GlobalContext"
+import { finalToast } from "./Hoc components/WithDelay";
 import 'animate.css';
 
-export function Toast(){
+function Toast({timeout}){
     const {toastMsg, setSeeToast, seeToast} =useContext(GlobalContext)
+    useEffect(() => {
+        timeout()
+    },[seeToast]);
+
     return(<>
         {seeToast ? <div className="animate__animated animate__fadeInRight flex fixed right-10 bottom-10 bg-slate-200 py-3 px-3 rounded border border-l-4 border-l-indigo-500 border-slate-950">
             <div>{toastMsg}</div>
@@ -12,3 +18,6 @@ export function Toast(){
         </>
     )
 }
+
+const HocedToast = finalToast(Toast)
+export { HocedToast }
